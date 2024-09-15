b061115804fef930ef3e90cefd168c5f716bdbe8
Bisola <abizmichelle@gmail.com>
2024-09-15 17:56:51 +0100

    server

---
+++ src/pages/ContactPage.tsx
@@ -0,0 +1,12 @@
+import ContactForm from "./ContactForm";
+
+const ContactPage = () => {
+  return (
+    <div>
+      <h1>Contact Page</h1>
+      <ContactForm />
+    </div>
+  );
+};
+
+export default ContactPage;
